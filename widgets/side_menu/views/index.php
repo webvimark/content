1a9076160ba110ad202c0f0aa1fc678e3fd753e3
vi mark <webvimark@gmail.com>
2014-09-17 22:39:43 +0200

    side menu widget from nav to menu

--- widgets/side_menu/views/index.php
+++ widgets/side_menu/views/index.php
@@ -3,12 +3,17 @@
  * @var $this yii\web\View
  */
 use webvimark\modules\content\models\Page;
-use yii\bootstrap\Nav;
+use yii\widgets\Menu;
+
 ?>
 
 <div class="side-menu-widget">
-	<?= Nav::widget([
+	<?= Menu::widget([
 		'items'=>Page::getItemsForMenu($this->context->place),
 		'activateParents'=>true,
+		'encodeLabels'=>false,
+		'options'=>[
+			'class'=>'nested-menu',
+		],
 	]) ?>
 </div>
